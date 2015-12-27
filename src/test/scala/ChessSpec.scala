f34d0f6a3ee5b4ad1528b912a052f68f091aad20
Jean Vilver <jean.vilver@gmail.com>
2015-12-27 16:08:45 +0100

    Refactor

--- src/test/scala/ChessSpec.scala
+++ src/test/scala/ChessSpec.scala
@@ -6,93 +6,87 @@ class ChessSpec extends FunSpec with Matchers  {
   describe("white pawn") {
     describe("move") {
       it("should be 1 square ahead") {
-        val board = """
-          1   ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
-          2   ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
-          3   . . . . . . . .
-          4   . . . . . . . .
-          5   . . . . . . . .
-          6   . . . . . . . .
-          7   ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
-          8   ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖
+        val result = {
+          val b = """
+            1   ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+            2   ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
+            3   . . . . . . . .
+            4   . . . . . . . .
+            5   . . . . . . . .
+            6   . . . . . . . .
+            7   ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
+            8   ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖
 
-              a b c d e f g h
-        """
-        val cg = ChessGame(board, White)
-        val result = ChessGame.move(cg, Move('d', 2, 'd', 3))
-        val expectedBoard = """
-          1   ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
-          2   ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
-          3   . . . . . . . .
-          4   . . . . . . . .
-          5   . . . . . . . .
-          6   . . . ♙ . . . .
-          7   ♙ ♙ ♙ . ♙ ♙ ♙ ♙
-          8   ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖
+                a b c d e f g h
+          """
+          ChessGame.move(ChessGame(b, White), Move('d', 2, 'd', 3))
+        }
+        val expected = {
+          val b = """
+            1   ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+            2   ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
+            3   . . . . . . . .
+            4   . . . . . . . .
+            5   . . . . . . . .
+            6   . . . ♙ . . . .
+            7   ♙ ♙ ♙ . ♙ ♙ ♙ ♙
+            8   ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖
 
-              a b c d e f g h
-        """
-        val expectedChessGame = ChessGame(expectedBoard, Black)
-        result should be (Right(expectedChessGame))
+                a b c d e f g h
+          """
+          ChessGame(b, Black)
+        }
+        result should be (Right(expected))
       }
       it("should be 2 squares ahead"){
-        val board = """
-          1   ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
-          2   ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
-          3   . . . . . . . .
-          4   . . . . . . . .
-          5   . . . . . . . .
-          6   . . . . . . . .
-          7   ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
-          8   ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖
+        val result = {
+          val b = """
+            1   ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+            2   ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
+            3   . . . . . . . .
+            4   . . . . . . . .
+            5   . . . . . . . .
+            6   . . . . . . . .
+            7   ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
+            8   ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖
 
-              a b c d e f g h
-        """
-        val cg = ChessGame(board, White)
-        val result = ChessGame.move(cg, Move('d', 2, 'd', 4))
-        val expectedBoard = """
-          1   ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
-          2   ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
-          3   . . . . . . . .
-          4   . . . . . . . .
-          5   . . . ♙ . . . .
-          6   . . . . . . . .
-          7   ♙ ♙ ♙ . ♙ ♙ ♙ ♙
-          8   ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖
+                a b c d e f g h
+          """
+          ChessGame.move(ChessGame(b, White), Move('d', 2, 'd', 4))
+        }
+        val expected = {
+          val b = """
+            1   ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+            2   ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
+            3   . . . . . . . .
+            4   . . . . . . . .
+            5   . . . ♙ . . . .
+            6   . . . . . . . .
+            7   ♙ ♙ ♙ . ♙ ♙ ♙ ♙
+            8   ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖
 
-              a b c d e f g h
-        """
-        val expectedChessGame = ChessGame(expectedBoard, Black)
-        result should be (Right(expectedChessGame))
+                a b c d e f g h
+          """
+          ChessGame(b, Black)
+        }
+        result should be (Right(expected))
       }
       it("should not be 3 squares ahead"){
-        val board = """
-          1   ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
-          2   ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
-          3   . . . . . . . .
-          4   . . . . . . . .
-          5   . . . . . . . .
-          6   . . . . . . . .
-          7   ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
-          8   ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖
+        val result = {
+          val b = """
+            1   ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
+            2   ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
+            3   . . . . . . . .
+            4   . . . . . . . .
+            5   . . . . . . . .
+            6   . . . . . . . .
+            7   ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
+            8   ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖
 
-              a b c d e f g h
-        """
-        val cg = ChessGame(board, White)
-        val result = ChessGame.move(cg, Move('d', 2, 'd', 5))
-        val expectedBoard = """
-          1   ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
-          2   ♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟
-          3   . . . . . . . .
-          4   . . . ♙ . . . .
-          5   . . . . . . . .
-          6   . . . . . . . .
-          7   ♙ ♙ ♙ . ♙ ♙ ♙ ♙
-          8   ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖
-
-              a b c d e f g h
-        """
-        val expectedChessGame = ChessGame(expectedBoard, White)
+                a b c d e f g h
+          """
+          ChessGame.move(ChessGame(b, White), Move('d', 2, 'd', 5))
+        }
         result should be ('left)
       }
     }
