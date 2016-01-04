708452d8d1d253026071e5ee6433de6aa63af337
Jean Vilver <jean.vilver@gmail.com>
2016-01-04 14:41:54 +0100

    Fix test

--- src/test/scala/QueenSpec.scala
+++ src/test/scala/QueenSpec.scala
@@ -474,14 +474,14 @@ class QueenSpec extends FunSpec with Matchers  {
 
     val result = {
       val b = """
-          8   . . . ♛ ♚ .  .
+          8   . . . ♛ ♚ . . .
           7   . . . . . . . .
           6   . . . . . . . .
           5   . . . . . . . .
           4   . . . ♕ . . . .
           3   . . . . . . . .
           2   . . . ♔ . . . .
-          1   . . . . . .  .
+          1   . . . . . . . .
 
               a b c d e f g h
         """
